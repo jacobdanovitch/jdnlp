still doing the stupid incorrect gradients

jdnlp/models/HierarchicalAttentionNetwork.py
@@ -92,7 +92,7 @@ class HierarchialAttentionNetwork(Model):
         
         weights = torch.FloatTensor(loss_weights)
         self.loss = nn.CrossEntropyLoss(weight=weights)
-        # self.loss = nn.NLLLoss(weight=torch.tensor(loss_weights)) # , device=device
+        # self.loss = nn.NLLLoss(weight=weights)
 
         initializer(self)
 
@@ -124,14 +124,11 @@ class HierarchialAttentionNetwork(Model):
             A scalar loss to be optimised.
         """
         embedded = self.text_field_embedder(tokens)
-        logger.warn(f"Word vecs: {embedded.size()}")
         
-        mask = util.get_text_field_mask(tokens, num_wrapping_dims=1)
+        # mask = util.get_text_field_mask(tokens, num_wrapping_dims=1)
         # logger.critical(f"MASK: {mask}")
-
-        doc_vecs, sent_attention, word_attention = self.encoder(embedded, mask)
-        logits = self.classifier_feedforward(doc_vecs) # .unsqueeze(-1)
-        logger.warn(f"Logits: {logits.size()}")
+        doc_vecs, sent_attention, word_attention = self.encoder(embedded, None)
+        logits = self.classifier_feedforward(doc_vecs)
 
         output_dict = {
             'logits': logits,
@@ -140,6 +137,10 @@ class HierarchialAttentionNetwork(Model):
         }
         if label is not None:
             loss = self.loss(logits, label)
+            # logger.critical(dir(loss))
+            # loss.backward(retain_graph=True)
+            # logger.critical(loss.grad)
+            # assert False
             for metric in self.metrics.values():
                 metric(logits, label)
             output_dict["loss"] = loss
@@ -154,4 +155,5 @@ class HierarchialAttentionNetwork(Model):
             "f1": self.metrics["f1"].get_metric(reset=reset)[2],
             "accuracy": self.metrics["accuracy"].get_metric(reset=reset)
         }
+
 

jdnlp/modules/Attention/Attention.py
@@ -8,8 +8,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 def new_parameter(*size):
-    out = Parameter(torch.FloatTensor(*size))
-    torch.nn.init.xavier_normal_(out)
+    out = nn.Parameter(torch.randn(*size, requires_grad=True).float())
+    nn.init.xavier_normal_(out)
     return out
 
 
@@ -18,6 +18,7 @@ class Attention(nn.Module):
     def __init__(self, attention_size):
         super().__init__()
         self.attention = new_parameter(attention_size, 1)
+        self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x_in, reduction_dim=-2, return_attn_distribution=False):
         """
@@ -30,80 +31,14 @@ class Attention(nn.Module):
         In two steps we convert [batch, sent_len, num_words_in_category, num_categories] into [batch, num_categories]
         """
         # calculate attn weights
-        attn_score = torch.matmul(x_in, self.attention).squeeze()
+        attn_score = x_in.matmul(self.attention)
         # add one dimension at the end and get a distribution out of scores
-        attn_distrib = F.softmax(attn_score.squeeze(), dim=-1).unsqueeze(-1)
+        attn_distrib = self.softmax(attn_score.squeeze()).unsqueeze(-1)
         scored_x = x_in * attn_distrib
         if reduction_dim:
-            scored_x = torch.sum(scored_x, dim=reduction_dim)
+            scored_x = scored_x.sum(dim=reduction_dim)
         
         if return_attn_distribution:
             return scored_x, attn_distrib.reshape(x_in.shape[0], -1)
 
         return scored_x
-
-
-class _Attention(nn.Module):
-
-    def __init__(self, hidden_size, attention_size):
-        super(Attention, self).__init__()
-
-        self.linear = nn.Linear(hidden_size, attention_size, bias=False)
-        self.tanh = nn.Tanh()
-        self.softmax = nn.Softmax(dim=-1)
-
-        ## Context vector
-        self.context_weight = nn.Parameter(torch.Tensor(attention_size, 1))
-        self.context_weight.data.normal_(mean=0.0, std=0.02)
-
-    def forward(self, h_src, mask=None):
-        # |h_src| = (batch_size, length, hidden_size)
-        # |mask| = (batch_size, length)
-        print("hsrc:", h_src.size())
-        batch_size, length, hidden_size = h_src.size()
-
-        # Resize hidden_vectors to generate weight
-        weights = h_src.view(-1, hidden_size)
-        weights = self.linear(weights)
-        weights = self.tanh(weights)
-
-        weights = torch.mm(weights, self.context_weight).view(batch_size, -1)
-        # |weights| = (batch_size, length)
-
-        if mask is not None:
-            # Set each weight as -inf, if the mask value equals to 1.
-            # Since the softmax operation makes -inf to 0, masked weights would be set to 0 after softmax operation.
-            # Thus, if the sample is shorter than other samples in mini-batch, the weight for empty time-step would be set to 0.
-            weights.masked_fill_(mask, -float('inf'))
-
-        # Modified every values to (0~1) by using softmax function
-        weights = self.softmax(weights)
-        # |weights| = (batch_size, length)
-
-        context_vectors = torch.bmm(weights.unsqueeze(1), h_src)
-        # |context_vector| = (batch_size, 1, hidden_size)
-
-        context_vectors = context_vectors.squeeze(1)
-        # |context_vector| = (batch_size, hidden_size)
-
-        return context_vectors, weights
-
-
-    @staticmethod
-    def generate_mask(length):
-            mask = []
-
-            max_length = max(length)
-            for l in length:
-                if max_length - l > 0:
-                    # If the length is shorter than maximum length among samples,
-                    # set last few values to be 1s to remove attention weight.
-                    mask += [torch.cat(
-                        [torch.zeros((1, l)).byte(), torch.ones((1, (max_length - l))).byte()],
-                        dim=-1)]
-                else:
-                    # If the length of the sample equals to maximum length among samples,
-                    # set every value in mask to be 0.
-                    mask += [torch.zeros((1, l)).byte()]
-
-            mask = torch.cat(mask, dim=0).bool()

jdnlp/modules/Attention/HierarchicalAttention.py
@@ -17,6 +17,7 @@ from copy import deepcopy
 
 import logging
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.CRITICAL)
 
 """
 input_size: int, 
@@ -35,32 +36,18 @@ class HierarchialAttention(Seq2VecEncoder):
     ):
         super().__init__()
 
-        """
-        self.word_attention_model = WordAttention(input_size=input_size,
-                                                  hidden_size=hidden_size,
-                                                  attention_size=attention_size,
-                                                  n_layers=n_layers,
-                                                  dropout_p=dropout_p,
-                                                  ).to(device)
-
-        self.sentence_attention_model = SentenceAttention(input_size=hidden_size,
-                                                          hidden_size=hidden_size,
-                                                          attention_size=attention_size,
-                                                          n_layers=n_layers,
-                                                          dropout_p=dropout_p,
-                                                          ).to(device)
-
-        self.device = device
-        """
-
         self.input_dim = word_encoder.get_input_dim()
         self.output_dim = sent_encoder.get_output_dim()
 
         self.word_encoder = TimeDistributed(word_encoder)
         self.sent_encoder = sent_encoder
 
-        self.word_attn = Attention(word_encoder.get_output_dim())
-        self.sent_attn = Attention(sent_encoder.get_output_dim())
+        self.word_attn = Attention(self.input_dim)
+        self.sent_attn = Attention(self.output_dim)
+        
+        self.sent_enc_grad = 0
+        self.params = None
+        # logging.critical(list(sent_encoder.named_parameters()))
 
     @overrides
     def forward(self, document: torch.Tensor, mask: torch.Tensor):
@@ -87,20 +74,32 @@ class HierarchialAttention(Seq2VecEncoder):
         """
 
         # mask is broken
-        sentence_vecs = self.word_encoder(document, mask=mask)
-        logger.warn(f"Word encodings: {sentence_vecs.size()}")
+        
+        sentence_vecs = self.word_encoder(document, mask=None)
+        # logger.warn(f"Word encodings: {sentence_vecs.size()}")
         
         sentence_vecs, word_weights = self.word_attn(sentence_vecs, return_attn_distribution=True) # self.word_attention_model(document, mask)
-        logger.warn(f"Word-attn: {sentence_vecs.size()}")
-        logger.warn(f"Word weights: {word_weights.size()}\n")
+        # logger.warn(f"Word-attn: {sentence_vecs.size()}")
+        # logger.warn(f"Word weights: {word_weights.size()}\n")
 
-        doc_vecs = self.sent_encoder(sentence_vecs, mask=None)
+        doc_vecs = self.sent_encoder(sentence_vecs, mask=None)# .sum(dim=1)
         # doc_vecs = self.word_encoder(sentence_vecs.unsqueeze(0), mask=None)
-        logger.warn(f"Sent encodings: {doc_vecs.size()}")
+        # logger.warn(f"Sent encodings: {doc_vecs.size()}")
 
+        # sentence_weights = None
         doc_vecs, sentence_weights = self.sent_attn(sentence_vecs, return_attn_distribution=True) # sentence_attention_model , reduction_dim=-1
         logger.warn(f"Doc vecs: {doc_vecs.size()}")
-        logger.warn(f"Sent weights: {sentence_weights.size()}")
+        # logger.warn(f"Sent weights: {sentence_weights.size()}")
+        
+        #  if name == '_output_projection.weight'
+        # self.sent_enc_grad += sum([parameter.grad if parameter is None else 0 for name, parameter in self.word_encoder.named_parameters()])
+        params = list(self.word_encoder.parameters())
+        if self.params is not None and params == self.params:
+            logger.critical(True)
+        self.params = params
+        # if torch.rand(1).item() > 0.75:
+            # logger.critical(list(self.word_encoder.parameters()))
+            # logger.critical(self.sent_enc_grad)
 
         return doc_vecs, sentence_weights, word_weights
 